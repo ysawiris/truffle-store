e3004169da516540095855d2cc39a46939d3bf2f
ysawiris <youssef.sawiris@students.dominican.edu>
2020-10-09 20:15:27 -0700

    added to ropsten network

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,17 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = "claim decade pave soul print sea margin multiply aerobic cart bunker purchase example retreat city";
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/593c0ba89c3240c691d4c97d02fe6239", 2)
+      },
+      network_id: 3,
+      gas: 4500000,
+      gasPrice: 10000000000,
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
